eeb0130c7497e102ed2e8c451a9ce6aa94b099d6
howdyfrom2019 <tjd1emd@naver.com>
2022-11-08 00:31:19 +0900

    gallery wall created

--- components/gallery.tsx
+++ components/gallery.tsx
@@ -1,8 +1,10 @@
 import * as Three from "three";
-import {useCallback, useEffect, useRef} from "react";
+import {useCallback, useEffect, useRef, useState} from "react";
 import {OrbitControls} from "three/examples/jsm/controls/OrbitControls";
 
 export default function Gallery() {
+  const [frameCount, setFrameCount] = useState(2);
+  const distance = useRef<number>(100);
   const canvasRef = useRef<HTMLDivElement>(null);
   const scene = useRef<Three.Scene>(new Three.Scene());
   const camera = useRef<Three.PerspectiveCamera>(new Three.PerspectiveCamera(80, window.innerWidth /window.innerHeight, 0.1, 10000));
@@ -19,10 +21,25 @@ export default function Gallery() {
     const axes = new Three.AxesHelper(150);
     const gridHelper = new Three.GridHelper(240, 20);
     scene.current.add(axes, gridHelper);
+    camera.current.position.set(0, 20, 150);
 
     const light = new Three.HemisphereLight(0xffffff, 0x080820, 0.8);
     light.position.set(0, 50, 50);
     scene.current.add(light);
+    const galleryGroup = new Three.Group();
+
+    {
+      const wallWidth: number = frameCount * distance.current;
+      const geometry = new Three.BoxGeometry(wallWidth, 100, 2);
+      const material = new Three.MeshPhongMaterial({ color: 0x464946 });
+      const wallMesh = new Three.Mesh(geometry, material);
+
+      wallMesh.position.set(wallWidth / 2, 0, -1.5);
+      wallMesh.receiveShadow = true;
+      wallMesh.castShadow = true;
+      galleryGroup.add(wallMesh);
+      scene.current.add(galleryGroup);
+    }
   }, []);
 
   const animate = useCallback(() => {
